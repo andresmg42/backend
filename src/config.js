fd7da29575bbf2afdb6bc6c69a1f3a5588f02bd0
andresmg42ortega <andresdavid.ortega@gmail.com>
2021-11-30 17:22:09 -0500

    cambio config.js

--- src/config.js
+++ src/config.js
@@ -10,10 +10,10 @@
  export default {
    database: {
      connectionLimit: 10,
-     host: process.env.DATABASE_HOST || "localhost",
-     user: process.env.DATABASE_USER || "root",
-     password: process.env.DATABASE_PASSWORD || "",
-     database: process.env.DATABASE_NAME || "appservices",
+     host: process.env.DATABASE_HOST || "btxvcu2oohxjmg5thqyo-mysql.services.clever-cloud.com",
+     user: process.env.DATABASE_USER || "ut5iz1jcvs6h2zjx",
+     password: process.env.DATABASE_PASSWORD || "a6rCeN6CEUncXTMjcyhG",
+     database: process.env.DATABASE_NAME || "btxvcu2oohxjmg5thqyo",
    },
    port: process.env.PORT || 4010,
    SECRET: 'RMS-WebApp-API'
